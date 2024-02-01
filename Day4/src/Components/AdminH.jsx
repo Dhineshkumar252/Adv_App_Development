732d3b7f462e3ae00f9f4ba9005ec4de92eca343
Dhineshkumar252 <114741243+Dhineshkumar252@users.noreply.github.com>
2024-02-01 12:10:12 +0530

    Add files via upload

---
+++ Day4/src/Components/AdminH.jsx
@@ -0,0 +1,31 @@
+import { FaReact } from "https://esm.sh/react-icons/fa";
+import React from "react";
+import './Navbar.scss';
+import './Caru.css';
+import Caru from "./Caru";
+import Footer from "./Footer.jsx";
+import { Button } from "react-bootstrap";
+import Nav from "./Nav";
+import AdminNav from "./AdminNav";
+import AdminFooter from "./AdminFooter";
+import AdminCaru from "./AdminCaru";
+// import { Button } from "@mui/material";
+// import { ButtonGroup } from "react-bootstrap";
+
+export default function AdminH() {
+	
+  
+	return (
+    <div className="App">
+		             <AdminNav/>
+					<div className="caru">
+
+					<AdminCaru/>
+					</div>
+					<div className="footer">
+                        <AdminFooter/>
+					</div>
+		</div>
+	);
+}
+
